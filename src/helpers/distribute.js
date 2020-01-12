67339f03418bc8dc772c4b9561a308997f100e55
Achim Kramer <achim@zibra.de>
2020-01-12 11:46:19 +0100

    Enhance distribution algorithm

--- src/helpers/distribute.js
+++ src/helpers/distribute.js
@@ -1,5 +1,19 @@
 module.exports = (amount, keys) => {
 
+    // Use simple round robin distribution
+//    var result = roundRobin(amount, keys);
+    var result = randomDistribution(amount, keys);
+
+    return result;
+}
+
+/**
+ * Round robin distribution
+ * @param {*} amount 
+ * @param {*} keys 
+ */
+var roundRobin = (amount, keys) => {
+
     // Initialize result set
     result = [];
     for(let k = 0; k < amount; k++) {
@@ -16,6 +30,50 @@ module.exports = (amount, keys) => {
 
         // Increase counter
         i++;
+    }
+
+    return result;
+}
+
+/**
+ * Method does a random disribution
+ * @param {*} amount 
+ * @param {*} keys 
+ */
+var randomDistribution = (amount, keys) => {
+
+    // Calculate max group size
+    var maxElements = Math.ceil(keys.length / amount);
+
+    // Initialize result set
+    var pids = [];      // ...pocket indices
+    var result = [];
+    for(let k = 0; k < amount; k++) {
+        result.push([]);
+        pids.push(k);   // e.g.: 0, 1, 2, 3...
+    }
+
+    var i = 0;
+    while(i < keys.length) {
+
+        // Get random pocket number
+        var randomPocket = Math.floor(Math.random() * pids.length);  
+
+        // Random Pocket index
+        var rpidx = pids[randomPocket];
+
+        // Check if pocket is already full
+        if(result[rpidx].length < maxElements) {
+
+            // Push key into random pocket
+            result[rpidx].push(keys[i]);
+
+            i++;
+        } else {
+
+            // Remove full pocket from random selection
+            pids.splice(randomPocket, 1);
+        }
     }
 
     return result;
