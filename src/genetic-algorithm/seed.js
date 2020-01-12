8761505d899219d1bf942c681fae63de314a59de
Achim Kramer <achim@zibra.de>
2020-01-12 11:59:20 +0100

    Enhance seeding function

--- src/genetic-algorithm/seed.js
+++ src/genetic-algorithm/seed.js
@@ -11,7 +11,7 @@ module.exports = (keys, groupAmount) => {
 
     // Check if it is possible to create a reasonable seed with given combination
     // Minimum Group size is 2
-    if(Math.floor(keys.length / groupAmount) >= minGroupSize) {
+    if(Math.floor(keys.length / groupAmount) < minGroupSize) {
         throw new Error('It is not possible to create a reasonable Chromosome seed with these given parameters: '+keys.length+' Items; '+groupAmount+' Groups');
     }
 
@@ -19,10 +19,7 @@ module.exports = (keys, groupAmount) => {
     var chromosome = [];
 
     // Initialize Group counter for constraint check
-    var counter = {};
-    for(let groupNr = 1; groupNr <= groupAmount; groupNr++) {
-        counter[groupNr] = 0;
-    }
+    counter = initGroupCounter(groupAmount);
 
     var pointer = 0;
     while(pointer < keys.length) {
@@ -42,8 +39,34 @@ module.exports = (keys, groupAmount) => {
             // Move chromosome pointer
             pointer++;
         }
+
+        // Do a constraint check in final round
+        if(pointer == (keys.length -1)) {
+
+            // Check if there aren't groups with just one member
+            Object.keys(counter).forEach((groupNr) => {
+                if(counter[groupNr] < minGroupSize) {
+                    // Resize group counter
+                    counter = initGroupCounter(groupAmount);
+
+                    // and start at the chromosome beginning
+                    pointer = 0;
+                }
+            });
+        }
     }
 
     // e.g.: [1, 1, 1, 3, 2, 2, 3, 3, 2, 2]
     return chromosome; 
+}
+
+/**
+ * This function initializes a fresh indexed group counter
+ */
+var initGroupCounter = (groupAmount) => {
+    var counter = {};
+    for(let groupNr = 1; groupNr <= groupAmount; groupNr++) {
+        counter[groupNr] = 0;
+    }
+    return counter;
 }
