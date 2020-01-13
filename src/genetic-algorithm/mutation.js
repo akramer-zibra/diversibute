ecb4943425072e3f4fa0cfe404060730268764a6
Achim Kramer <achim@zibra.de>
2020-01-14 00:02:30 +0100

    Remove debug output and simplify code

--- src/genetic-algorithm/mutation.js
+++ src/genetic-algorithm/mutation.js
@@ -13,7 +13,6 @@ var mutate = (chromosome) => {
 
     // Select a random chromosome bit
     var randomPtr = Math.floor(Math.random() * (chromosome.length - 1 - 0 + 1)) + 0; // @see https://stackoverflow.com/a/1527820
-    console.log(randomPtr);
     
     /* Increase one bit and prevent "zero" value */
     chromosome[randomPtr] = ((chromosome[randomPtr] + 1) % numberGroups) + 1; 
@@ -28,16 +27,7 @@ module.exports = (chromosome) => {
 
     // Clone chromosome
     var clone = chromosome.slice(0);
-    console.log(clone);
 
     // Mutate given chromosome
-    clone = mutate(clone);
-
-    // FIXME.. move constraint test into fitness function!!!
-    // Mutate until constraints are valid
-    /*while(!constraint(clone)) {
-        clone = mutate(clone);
-    }*/
-
-    return clone;
+    return mutate(clone);
 }
