17854ab3e61734d5cc2d5bce218ab9bdbd8d5d47
Achim Kramer <achim@zibra.de>
2020-01-13 14:03:58 +0100

    Add mutation function to project

---
+++ src/genetic-algorithm/mutation.js
@@ -0,0 +1,43 @@
+const constraint = require('../constraints/chromosome');
+const counter = require('../helpers/counter');
+
+/**
+ * Internal method mutates given chromosome
+ * @param {*} chromosome 
+ */
+var mutate = (chromosome) => {
+
+    // Count group members
+    var counts = counter(chromosome);
+    var numberGroups = Object.keys(counts).length;
+
+    // Select a random chromosome bit
+    var randomPointer = Math.floor(Math.random() * (chromosome.length - 1 - 0 + 1)) + 0; // @see https://stackoverflow.com/a/1527820
+    console.log(randomPointer);
+    
+    /* Increase one bit */
+    chromosome[randomPointer] = ((chromosome[randomPointer] + 1) % numberGroups) + 1; 
+    
+    return chromosome;
+}
+
+/**
+ * This function creates a valid mutation of given chromosome
+ */
+module.exports = (chromosome) => {
+
+    // Clone chromosome
+    var clone = chromosome.slice(0);
+    console.log(clone);
+
+    // Mutate given chromosome
+    clone = mutate(clone);
+
+    // TODO.. move constraint test into fitness function!!!
+    // Mutate until constraints are valid
+    /*while(!constraint(clone)) {
+        clone = mutate(clone);
+    }*/
+
+    return clone;
+}
