c08994f8c9f9d67358c6c6a7e0b3ee3fe2bb21e7
Achim Kramer <achim@zibra.de>
2020-01-14 12:09:37 +0100

    Fix ga algorithm

--- index.js
+++ index.js
@@ -18,21 +18,34 @@ var population = [];
 for(let n = 0; n < config.initialSeeds; n++) {
     population.push(seedFunc(keys, config.amountGroups));
 }
+/*
 console.log(population[0]);
 console.log(population[1]);
 
 // Test mutation
 console.log('Mutation...');
-console.log(mutationFunc(population[0]));
+var mutated = mutationFunc(population[0])
+console.log(mutated);
 
 // Test Crossover
 console.log('Crossover...');
-console.log(population[0]);
-console.log(population[1]);
-console.log('+++');
-console.log(crossoverFunc(population[0], population[1]));
+var crossed = crossoverFunc(population[0], population[1]);
+console.log(crossed);
 
-/*
+// Test fintess
+console.log('Fitness...');
+console.log(population[0]);
+console.log(fitnessFunc(population[0]));
+console.log(population[1]);
+console.log(fitnessFunc(population[1]));
+console.log(crossed[0]);
+console.log(fitnessFunc(crossed[0]));
+console.log(crossed[1]);
+console.log(fitnessFunc(crossed[1]));
+console.log(mutated);
+console.log(fitnessFunc(mutated));
+*/
+
 // Configure genetic algorithm
 var gaConfig = {
     mutationFunction: mutationFunc,
@@ -45,11 +58,17 @@ var gaConfig = {
 // Create a fresh algorithm object here
 const geneticAlgorithm = Genetics(gaConfig);
 
+console.log(geneticAlgorithm.scoredPopulation());
+
 // Start algorithm
-for(let c = 0; c <= 20; c++) {
+for(let c = 0; c <= 10; c++) {
     geneticAlgorithm.evolve(10).then((result) => {
-        console.log('Evolution: '+ c*10);
-        console.log(result.best()[0].score);
+//        console.log('Evolution: '+ c*10);
+//        console.log(result.best()[0].score);
+
+        // Print final population in last round
+        if(c == 10) {
+            console.log(result.scoredPopulation());
+        }
     });
 }
-*/
