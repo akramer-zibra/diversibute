c5aa23664c5fa14dec4fda7f936bced112c630ce
Achim Kramer <achim@zibra.de>
2020-01-15 11:21:56 +0100

    Add best known combinations as comments

--- data.js
+++ data.js
@@ -1,9 +1,9 @@
-const inputXS = require('./examples/data/input-xs.json');
-const inputS = require('./examples/data/input-s.json');
+const inputXS = require('./examples/data/input-xs.json');   // Best known score is: 4.438407168565259
+const inputS = require('./examples/data/input-s.json');     // Best known score is: 42.46570983903612
 
 /**
  * This function is a facade for reading input data 
  */
 module.exports = () => {
-    return inputXS;
+    return inputS;
 }
