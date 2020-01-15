74869bdc69356020cfe01a79646b6df61343d921
Achim Kramer <achim@zibra.de>
2020-01-15 12:08:27 +0100

    Fix testdata module require

--- data.js
+++ data.js
@@ -1,10 +1,11 @@
-const inputXS = require('./examples/data/input-xs.json');   // Best known score is: 4.438407168565259
-const inputS = require('./examples/data/input-s.json');     // Best known score is: 42.46570983903612
-const inputM = require('./examples/data/input-m.json');     // Best known score is: 233.4154995819719
+const inputXS = require('./examples/data/3features/input-xs.json');   // Best known score is: 4.438407168565259
+const inputS = require('./examples/data/3features/input-s.json');     // Best known score is: 42.46570983903612
+const inputM = require('./examples/data/3features/input-m.json');     // Best known score is: 233.4154995819719
+const inputL = require('./examples/data/3features/input-l.json');     // Best known score is: 591.3447615796198
 
 /**
  * This function is a facade for reading input data 
  */
 module.exports = () => {
-    return inputM;
+    return inputL;
 }
