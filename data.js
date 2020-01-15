0ad451be061a9a252c3dbd5df96e459d74ba5f2d
Achim Kramer <achim@zibra.de>
2020-01-15 11:33:34 +0100

    Add medium size testdata to project

--- data.js
+++ data.js
@@ -1,9 +1,10 @@
 const inputXS = require('./examples/data/input-xs.json');   // Best known score is: 4.438407168565259
 const inputS = require('./examples/data/input-s.json');     // Best known score is: 42.46570983903612
+const inputM = require('./examples/data/input-m.json');     // Best known score is: 233.4154995819719
 
 /**
  * This function is a facade for reading input data 
  */
 module.exports = () => {
-    return inputS;
+    return inputM;
 }
