b87f07f7421ce123e4ab7cb088e3ffec0c210ef6
Achim Kramer <achim@zibra.de>
2020-01-15 10:36:18 +0100

    Add ne module data

---
+++ data.js
@@ -0,0 +1,9 @@
+const inputXS = require('./examples/data/input-xs.json');
+const inputS = require('./examples/data/input-s.json');
+
+/**
+ * This function is a facade for reading input data 
+ */
+module.exports = () => {
+    return inputS;
+}
