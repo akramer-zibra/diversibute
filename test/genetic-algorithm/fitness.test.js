9081f25369581ed49e7d848340e4fc5b1216ecb6
Achim Kramer <achim@zibra.de>
2020-01-26 18:10:27 +0100

    Fix unit tests for fitness function

--- test/genetic-algorithm/fitness.test.js
+++ test/genetic-algorithm/fitness.test.js
@@ -9,7 +9,12 @@ beforeEach(function() {
     });
 
     mockery.registerAllowable('../../src/genetic-algorithm/fitness');
-    mockery.registerAllowables(['../helpers/difference', '../helpers/counter', 'js-combinatorics', 'euclidean-distance', './squared', '../constraints/chromosome']);
+    mockery.registerAllowables(['../helpers/difference', 
+                                '../helpers/counter', 
+                                'js-combinatorics', 
+                                'euclidean-distance', 
+                                './squared', 
+                                '../constraints/chromosome']);
 });
 
 afterEach(function() {
@@ -17,6 +22,26 @@ afterEach(function() {
 });
 
 describe("Fitness function", function() {
+  describe("Essential calculation", function() {
+    it("works with medium size example data", function() {
+
+        // Example input data
+        mockery.registerAllowable("../../examples/data/3features/input-m.json");
+        var input = require("../../examples/data/3features/input-m.json");
+
+        // Source under test
+        var fitness = require("../../src/genetic-algorithm/fitness");
+
+        // Set context for testing
+        fitness.context(input, 5);
+
+        // Use score function
+        var score = fitness.calc({seq: [4, 2, 5, 3, 4, 1, 3, 1, 4, 2, 1, 4, 5, 5, 5, 1, 1, 5, 3, 2, 2, 2, 1, 3, 3, 4]});
+
+        expect(score).to.be.an('number');
+    });
+  }); 
+
   describe("Check necessary prerequesites", function() {
     it("Throws Error if context is missing", function() {
         
@@ -24,7 +49,9 @@ describe("Fitness function", function() {
         var fitness = require("../../src/genetic-algorithm/fitness");
 
         // Fitness function should throw error in case of missing context
-        expect(fitness.calc({seq: []})).to.throw('error');
+        expect(function() {
+            fitness.calc({seq: []});
+        }).to.throw('Error');
     });
 
     it("Returns NEGATIVE_INFINITY in case of undefined argument", function() {
@@ -54,5 +81,4 @@ describe("Fitness function", function() {
         expect(fitness.calc({seq: []})).to.equal(Number.NEGATIVE_INFINITY);
     });    
   });
-  
 });
