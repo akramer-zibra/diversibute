108ece34e216314cf6ad08a2671175794171327f
Achim Kramer <achim@zibra.de>
2020-02-05 18:21:39 +0100

    Add a factory method to genetic algorithm code

---
+++ src/genetic-algorithm/index.js
@@ -0,0 +1,19 @@
+/**
+ * This is a factory Method for functions of the genetic algorithm
+ * 
+ * @param {Bottle} bottle BottleJS instance
+ */
+module.exports = {
+
+    /**
+     * This function builds necessary function dependencies for genetic algorithm
+     */
+    build: (bottle) => {
+
+        // Load genetic algorithm functions
+        bottle.constant('seed', require('./seed'));
+        bottle.constant('fitness', require('./fitness'));
+        bottle.constant('mutation', require('./mutation'));
+        bottle.constant('crossover', require('./crossover'));
+    }
+}
