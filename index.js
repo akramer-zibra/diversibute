2c363ff0ab7d05e7ca3338673f395876dbeb630c
Achim Kramer <achim@zibra.de>
2020-02-22 22:00:38 +0100

    Add input validation to high level api

--- index.js
+++ index.js
@@ -20,6 +20,9 @@ module.exports = {
    * @returns {Promise<{combination: Array<Number>, score: Number}>}
    */
   monteCarlo: (data, groups, settings = {}) => {
+    // Validate input arguments
+    validate(data, groups)
+
     // Wrap data- and groups-input into one input group
     var input = { data, groups }
 
@@ -35,11 +38,31 @@ module.exports = {
    * @returns {Promise<{combination: Array<Number>, score: Number}>}
    */
   genetic: (data, groups, settings = {}) => {
+    // Validate input arguments
+    validate(data, groups)
+
     // Wrap data- and groups-input into one input object
     var input = { data, groups }
 
     // Run algorithm with arguments
     return ga.run(input, settings)
+  }
+}
+
+/**
+ * Validates given input arguments
+ * @param {*} data
+ * @param {*} groups
+ */
+var validate = (data, groups) => {
+  //
+  if (groups < 2) {
+    throw new Error('Error: Number of groups must be at least 2. Given ' + groups)
+  }
+
+  // We need at least two elements for each group
+  if (Object.keys(data).length < (2 * groups)) {
+    throw new Error('Error: We need at least 2xgroups input elements. Given ' + Object.keys(data).length)
   }
 }
 
